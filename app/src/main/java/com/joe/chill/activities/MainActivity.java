46cd0504ac4303611b0c14bc33dcc6cad85c4519
Joe Cheatham <cheathamjoe@gmail.com>
2016-04-01 20:37:05 -0500

    start profile view

--- app/src/main/java/com/joe/chill/activities/MainActivity.java
+++ app/src/main/java/com/joe/chill/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.joe.chill;
+package com.joe.chill.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.joe.chill.R;
 
 public class MainActivity extends AppCompatActivity {
 
