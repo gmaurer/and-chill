858c6d204588ce0f18f4acca055d76c262e5e11d
Joe Cheatham <cheathamjoe@gmail.com>
2016-04-01 22:13:55 -0500

    color change and basic message layout

---
+++ app/src/main/java/com/joe/chill/activities/PrivateMessageActivity.java
@@ -0,0 +1,15 @@
+package com.joe.chill.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joe.chill.R;
+
+public class PrivateMessageActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_private_message);
+  }
+}
